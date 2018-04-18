624d60e92758532e0d29ed78b851c74042409656
Nikolay Bely <belyynikolay@gmail.com>
2018-04-19 06:37:20 +0700

    Solve AngryBirds task

--- AngryBirds/AngryBirdsTask.cs
+++ AngryBirds/AngryBirdsTask.cs
@@ -2,17 +2,15 @@
 
 namespace AngryBirds
 {
-	public static class AngryBirdsTask
-	{
-		//  Ниже — это XML документация, её использует ваша среда разработки, 
-		// чтобы показывать подсказки по использованию методов. 
-		// Но писать её естественно не обязательно.
-		/// <param name="v">Начальная скорость</param>
-		/// <param name="distance">Расстояние до цели</param>
-		/// <returns>Угол прицеливания в радианах от 0 до Pi/2</returns>
-		public static double FindSightAngle(double v, double distance)
-		{
-			return Math.PI / 4;
-		}
-	}
+    public static class AngryBirdsTask
+    {
+        //  Ниже — это XML документация, её использует ваша среда разработки, 
+        // чтобы показывать подсказки по использованию методов. 
+        // Но писать её естественно не обязательно.
+        /// <param name="v">Начальная скорость</param>
+        /// <param name="distance">Расстояние до цели</param>
+        /// <returns>Угол прицеливания в радианах от 0 до Pi/2</returns>
+        public static double FindSightAngle(double v, double distance) =>
+            0.5 * Math.Asin(9.8 * distance / Math.Pow(v, 2.0));
+    }
 }

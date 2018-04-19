83f4eb1d8d83141df6a0591208dbb7fa50d5221e
Nikolay Bely <belyynikolay@gmail.com>
2018-04-19 07:14:16 +0700

    Add unit test support to Percents

---
+++ Percents/PercentTaskTests.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace Percents
+{
+    [TestFixture]
+    public class PercentTaskTests
+    {
+        [TestCase("1000 24 3", 1061.208)]
+        public void CalculateTest(string userInput, double res)
+        {
+            Assert.That(PercentsTask.Calculate(userInput), Is.EqualTo(res));
+        }
+    }
+}

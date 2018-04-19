882d2c4dc7121ef9de15a9b48f78362aaf59d337
Nikolay Bely <belyynikolay@gmail.com>
2018-04-19 07:26:57 +0700

    Solve Percents task

--- Percents/PercentsTask.cs
+++ Percents/PercentsTask.cs
@@ -1,10 +1,18 @@
-﻿namespace Percents
+﻿using System;
+
+namespace Percents
 {
     public static class PercentsTask
     {
         public static double Calculate(string userInput)
         {
-            return 0.0;
+            var args = userInput.Split(' ');
+
+            double totalMoney = double.Parse(args[0]),
+                monthRate = double.Parse(args[1]) / 12,
+                termOfDeposit = double.Parse(args[2]);
+
+            return totalMoney * Math.Pow(1 + monthRate / 100, termOfDeposit);
         }
     }
 }

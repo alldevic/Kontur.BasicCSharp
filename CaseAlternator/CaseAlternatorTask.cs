0db089b194207c26494475a5da03f835f68a18af
Nikolay Bely <belyynikolay@gmail.com>
2018-04-21 05:29:56 +0700

    Add and solve CaseAlternator task

---
+++ CaseAlternator/CaseAlternatorTask.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+
+namespace CaseAlternator
+{
+    public class CaseAlternatorTask
+    {
+        public static List<string> AlternateCharCases(string lowercaseWord)
+        {
+            var result = new List<string>();
+            AlternateCharCases(lowercaseWord.ToCharArray(), -1, result);
+            return result;
+        }
+
+        static void AlternateCharCases(char[] word, int startIndex, List<string> result)
+        {
+            result.Add(new string(word));
+            for (var i = word.Length - 1; i > startIndex; i--)
+            {
+                if (!IsCorrectLetter(word[i]))
+                {
+                    continue;
+                }
+
+                word[i] = char.ToUpper(word[i]);
+                AlternateCharCases(word, i, result);
+                word[i] = char.ToLower(word[i]);
+            }
+        }
+
+        private static bool IsCorrectLetter(char letter) =>
+            ('a' <= letter) && (letter <= 'z') || ('A' <= letter) && (letter <= 'B');
+    }
+}
---
+++ CaseAlternator/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace CaseAlternator
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var strings = CaseAlternatorTask.AlternateCharCases("ab42");
+            foreach (var str in strings)
+            {
+                Console.WriteLine(str);
+            }
+        }
+    }
+}

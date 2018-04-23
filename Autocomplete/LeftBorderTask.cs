9d339d3b5f0e674bb4a7d751b4fecbf4ba5ebf36
Nikolay Bely <belyynikolay@gmail.com>
2018-04-23 07:04:04 +0700

    Solve LeftBorder task

--- Autocomplete/LeftBorderTask.cs
+++ Autocomplete/LeftBorderTask.cs
@@ -1,18 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 
 namespace Autocomplete
 {
-    // Внимание!
-    // Есть одна распространенная ловушка при сравнении строк: строки можно сравнивать по-разному:
-    // с учетом регистра, без учета, зависеть от кодировки и т.п.
-    // В файле словаря все слова отсортированы методом StringComparison.OrdinalIgnoreCase.
-    // Во всех функциях сравнения строк в C# можно передать способ сравнения.
     public class LeftBorderTask
     {
         /// <returns>
-        /// Возвращает индекс левой границы. То есть индекс максимальной фразы, которая не начинается с prefix и меньшая prefix.
+        /// Возвращает индекс левой границы.
+        /// То есть индекс максимальной фразы, которая не начинается с prefix и меньшая prefix.
         /// Если такой нет, то возвращает -1
         /// </returns>
         /// <remarks>
@@ -21,15 +16,16 @@ namespace Autocomplete
         /// </remarks>
         public static int GetLeftBorderIndex(IReadOnlyList<string> phrases, string prefix, int left, int right)
         {
-            // IReadOnlyList похож на List, но у него нет методов модификации списка.
-            // Этот код решает задачу, но слишком неэффективно. Замените его на бинарный поиск!
-            for (int i = 0; i < phrases.Count; i++)
+            if (left + 1 >= right)
             {
-                if (string.Compare(prefix, phrases[i], StringComparison.OrdinalIgnoreCase) < 0
-                    || phrases[i].StartsWith(prefix, StringComparison.OrdinalIgnoreCase))
-                    return i - 1;
+                return left;
             }
-            return phrases.Count-1;
+
+            var m = (left + right) / 2;
+
+            return string.Compare(phrases[m], prefix, StringComparison.OrdinalIgnoreCase) < 0
+                ? GetLeftBorderIndex(phrases, prefix, m, right)
+                : GetLeftBorderIndex(phrases, prefix, left, m);
         }
     }
 }
